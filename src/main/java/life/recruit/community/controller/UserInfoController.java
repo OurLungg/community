c0ca9f5c52d6da18b769e85248a979a2f40780a5
codelungg <33400549@qq.com>
2020-05-11 18:17:05 +0800

    完成个人资料

--- src/main/java/life/recruit/community/controller/UserInfoController.java
+++ src/main/java/life/recruit/community/controller/UserInfoController.java
@@ -1,13 +1,24 @@
 package life.recruit.community.controller;
 
+import life.recruit.community.model.User;
+import life.recruit.community.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class UserInfoController {
 
+
     @RequestMapping("/userInfo")
-    public String userInfo() {
+    public String userInfo(Model model, HttpServletRequest request) {
+        User tb_user = (User) request.getSession().getAttribute("tb_user");
+        model.addAttribute("tb_user", tb_user);
         return "userInfo";
     }
+
+
 }
